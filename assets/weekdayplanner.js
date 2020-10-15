d4d078bf756e7e60da5b9f519d395acc24dac3f3
tony913 <tonycrosby96@gmail.com>
2020-10-15 16:19:13 -0500

    updating working

--- assets/weekdayplanner.js
+++ assets/weekdayplanner.js
@@ -55,64 +55,50 @@ $(document).ready(function () {
   var hours = {
     amHours: [9, 10, 11],
     pmHours: [12, 13, 14, 15, 16, 17],
-    hour: [9, 10, 11, 12, 13, 14, 15, 16, 17],
   };
-  // defining the conversions.
-  var conversion = {
-    amHour: [9, 10, 11],
-    pmHour: [12, 13, 14, 15, 16, 17],
-  };
-
-  var hourColors = [9, 10, 11, 12, 13, 14, 15, 16, 17]
-
-
   // storing data into local storage
-  localStorage.setItem("hours", JSON.stringify(conversion));
+  localStorage.setItem("hours", JSON.stringify(hours));
 
   // loop to display the 9am - 11am
-  for (var i = 0; i < hours.amHours.length; i++) {
-    var newDivClassRow = $("<div class='row block'></div>");
-    $(".container").append(newDivClassRow);
-    var hourCol = $(`<div class="col-2 time-block">${hours.amHours[i]}:00</div>`);
-
-    var textCol = $(
-      `<textarea class="col description" placeholder='Enter your events here' id=${conversion.amHour[i]}></></textarea>`
-    );
-    var buttonCol = $(
-      `<button class="saveBtn styled" id=${hours.amHours[i]}><i class="fas fa-save"></i></button>`
-    );
-    $(newDivClassRow).append(hourCol, textCol, buttonCol);
-  }
-  // loop to display the 12pm - 5pm
-  for (var i = 0; i < hours.pmHours.length; i++) {
-    var newDivClassRow = $("<div class='row block'></div>");
-    $(".container").append(newDivClassRow);
-
-    var hourCol = $(`<div class='col-2 time-block'>${hours.pmHours[i]}:00</div>`);
-
-    var textCol = $(
-      `<textarea class="col description" placeholder='Enter your events here' id=${conversion.pmHour[i]}></textarea>`
-    );
-    var buttonCol = $(
-      `<button class="saveBtn styled" id=${hours.pmHours[i]}><i class="fas fa-save"></i></button>`
-    );
-    $(newDivClassRow).append(hourCol, textCol, buttonCol);
-  }
-  // storing data to local storage
-  $("[id]").each(function () {
-    $(this).val(localStorage.getItem($(this).attr("id")));
-  });
-  // storing data into local storage
-  $("[id]").on("keyup", function (itm) {
-    localStorage.setItem($(this).attr("id"), $(this).val().trim());
-  });
-
+  function dayAndTime() {
+    for (var i = 0; i < hours.amHours.length; i++) {
+      var newDivClassRow = $("<div class='row block'></div>");
+      $(".container").append(newDivClassRow);
+      var hourCol = $(`<div class="col-2 time-block">${hours.amHours[i]}:00</div>`);
+  
+      var textCol = $(
+        `<textarea class="col description" placeholder='Enter your events here' id=${hours.amHours[i]}></textarea>`
+      );
+      var buttonCol = $(
+        `<button class="saveBtn styled" id=${hours.amHours[i]}><i class="fas fa-save"></i></button>`
+      );
+      $(newDivClassRow).append(hourCol, textCol, buttonCol);
+    }
+    // loop to display the 12pm - 5pm
+    for (var i = 0; i < hours.pmHours.length; i++) {
+      var newDivClassRow = $("<div class='row block'></div>");
+      $(".container").append(newDivClassRow);
+  
+      var hourCol = $(`<div class='col-2 time-block'>${hours.pmHours[i]}:00</div>`);
+  
+      var textCol = $(
+        `<textarea class="col description" placeholder='Enter your events here' id=${hours.pmHours[i]}></textarea>`
+      );
+      var buttonCol = $(
+        `<button class="saveBtn styled" id=${hours.pmHours[i]}><i class="fas fa-save"></i></button>`
+      );
+      $(newDivClassRow).append(hourCol, textCol, buttonCol);
+    }
+  
+};
+dayAndTime();
+  
   function colorChange() {
     $('textarea').each(function () {
         var currentHour = moment().format("HH");
-        // var textData = $('textarea').data("id");
+        // var textData = $('textarea').data("#");
         $("#" + currentHour).addClass("present");
-        for (let i = 7; i < 18; i++) {
+        for (let i = 9; i < 18; i++) {
           if (currentHour < i) {
             $("#" + i).addClass("future");
           } else if (currentHour > i) {
@@ -124,4 +110,14 @@ $(document).ready(function () {
     });
 };
 colorChange();
+
+  function saveInput(){
+    var saveBtn = $('.saveBtn');
+    saveBtn.on('click', function(){
+        let eventId = $(this).attr('id');
+        let eventText = $(this).siblings('.description').val();
+        localStorage.setItem(eventId, eventText);
+    });
+  }
+saveInput();
 });
